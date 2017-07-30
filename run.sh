75e4ec808db721bd2ea1309a2c817ea74fccb09b
Abkreno <mohamedelhamamsy789@gmail.com>
2017-07-30 21:39:43 +0200

    Modify run script

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-source ~/.bashrc
-nvm use 6.9
 echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches  && echo 999999 | sudo tee -a  /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee  -a /proc/sys/fs/inotify/max_user_instances && watchman  shutdown-server
 react-native run-android
 adb -s emulator-5554 reverse tcp:3000 tcp:3000
